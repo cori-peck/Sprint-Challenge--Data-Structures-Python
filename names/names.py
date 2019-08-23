b81b58cfbf5fe971476346bf23b319ae2305752c
cori-peck <cori.peck@ymail.com>
2019-08-23 12:57:15 -0500

    names running at 0.059 seconds, task 2 complete

--- names/names.py
+++ names/names.py
@@ -10,10 +10,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_1.sort()
+names_2.sort()
+
 duplicates = []
-for name in names_1:
-    if name in names_2:
-        duplicates.append(name)
+a = 0
+b = 0
+while a < len(names_1) and b < len(names_2):
+    if names_1[a] == names_2[b]:
+        duplicates.append(names_1[a])
+        a += 1
+        b += 1
+    elif names_1[a] < names_2[b]:
+        a += 1
+    else:
+        b += 1
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
